2be3022a2666b5591e68871d67dedd28ad64a304
Mohamed Sarhan <mrs.mohamedsarhan2024@gmail.com>
2026-03-25 09:49:46 +0200

    connect db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
-
+import mongoose from "mongoose";
 const app = express();
 
-app.listen("5000", () => {
-  console.log("localhost:5000");
-});
+mongoose
+  .connect(process.env.URLDB)
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`localhost:${process.env.PORT || 5000}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
