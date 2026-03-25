25e29af6eb5d9905f370951db7fe63ee5ec34288
Mohamed Sarhan <mrs.mohamedsarhan2024@gmail.com>
2026-03-25 09:56:28 +0200

    create user.model

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { use } from "react";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "username is required"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
